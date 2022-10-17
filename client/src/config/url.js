0b0fefe0a938b6eac23cb2724b8c05c44bca5afd
KaiNguyen <nguyenduckhai8101@gmail.com>
2022-10-17 23:25:38 +0700

    add debounce for tag

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,2 +1,12 @@
-export const API_URL = "http://api.fosha.tk";
-// "https://api-fosha.herokuapp.com"
+import { DEVELOPMENT, PRODUCTION } from "./node.env";
+
+const setBaseUrl = () => {
+  const { NODE_ENV } = process.env;
+
+  if (NODE_ENV == DEVELOPMENT) return "http://localhost:3001";
+  if (NODE_ENV == PRODUCTION) return "http://api.fosha.tk";
+
+  return "http://localhost:3001";
+};
+
+export const API_URL = setBaseUrl();
